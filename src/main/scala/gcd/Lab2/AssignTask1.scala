f3340369ee90dcc6eb46756e41cfd34c355f52f1
Subulzaidi <subul9010@gmail.com>
2023-08-11 14:54:27 +0500

    lab2

--- src/main/scala/gcd/Lab2/AssignTask1.scala
+++ src/main/scala/gcd/Lab2/AssignTask1.scala
@@ -1,24 +1,30 @@
 package Lab2
-import chisel3 . _
-import chisel3 . util . _
+import chisel3._
+import chisel3.util._
 
 class LM_IO_Interface extends Bundle {
-val s0 = Input ( Bool () )
-val s1 = Input ( Bool () )
-val s2 = Input ( Bool () )
-val out = Output ( UInt (32. W ) )
+  val s0 = Input(Bool())
+  val s1 = Input(Bool())
+  val s2 = Input(Bool())
+  val out = Output(UInt(32.W))
 }
 class Mux_5to1 extends Module {
-val io = IO (new LM_IO_Interface )
+  val io = IO(new LM_IO_Interface)
 
 // Start coding here
-val sel=Cat(io.s2,io.s1,io.s0)
-io . out := MuxCase ( false .B , Array (
-    (sel === "b000".U) -> 0.U,
-    (sel === "b001".U) -> 8.U,
-    (sel === "b010".U) -> 16.U,
-    (sel === "b011".U) -> 24.U,
-    (sel === "b100".U) -> 32.U
-))
+  val sel = Cat(io.s2, io.s1, io.s0)
+  io.out := MuxCase(
+    false.B,
+    Array(
+      (sel === "b000".U) -> 0.U,
+      (sel === "b001".U) -> 8.U,
+      (sel === "b010".U) -> 16.U,
+      (sel === "b011".U) -> 24.U,
+      (sel === "b100".U) -> 32.U,
+      (sel === "b101".U) -> 32.U,
+      (sel === "b110".U) -> 32.U,
+      (sel === "b111".U) -> 32.U
+    )
+  )
 
 }
