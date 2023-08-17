169fd9c59f3562589dcda317418f297841fec0bf
Subulzaidi <subul9010@gmail.com>
2023-08-17 23:05:26 +0500

    lab3 completed

---
+++ src/test/scala/gcd/Lab3/Decodertester.scala
@@ -0,0 +1,17 @@
+package gcd.Lab3
+
+import chisel3._
+import chisel3.tester._
+import org.scalatest.FreeSpec
+import chisel3.experimental.BundleLiterals._
+class DecoderTester extends FreeSpec with ChiselScalatestTester {
+    "Testing" in{
+        test(new decoder_with_valid) {b=>
+            b.io.in.poke("b00".U)
+            b.io.out.expect()
+            b.clock.step(1)
+            }
+
+    }
+}
+
